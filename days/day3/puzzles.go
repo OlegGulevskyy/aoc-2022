6588e44fe915b30be0cc99108aed2d71a4d7d2b0
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-03 12:27:01 +0100

    second puzzle day 3

--- days/day3/puzzles.go
+++ days/day3/puzzles.go
@@ -14,11 +14,11 @@ func p1(in string) int {
 	var ans int
 
 	lines := strings.Split(in, "\n")
-	lines = lines[:len(lines) - 1]
+	lines = lines[:len(lines)-1]
 
 	for _, l := range lines {
 		div := len(l) / 2
-		
+
 		for i, c := range l {
 			char := fmt.Sprintf("%c", c)
 			if i >= div {
@@ -35,6 +35,43 @@ func p1(in string) int {
 
 func p2(in string) int {
 	var ans int
+
+	lines := strings.Split(in, "\n")
+	lines = lines[:len(lines)-1]
+	var group []string
+	var groups [][]string
+	for i, l := range lines {
+		group = append(group, l)
+		ind := i + 1
+		if ind%3 == 0 {
+			groups = append(groups, group)
+			group = []string{}
+		}
+	}
+
+GroupsLoop:
+	for _, grp := range groups {
+		for i, line := range grp {
+			for _, c := range line {
+				char := fmt.Sprintf("%c", c)
+				var firstHit bool
+				var secondHit bool
+
+				if strings.Index(grp[i+1], char) != -1 {
+					firstHit = true
+				}
+
+				if strings.Index(grp[i+2], char) != -1 {
+					secondHit = true
+				}
+
+				if firstHit && secondHit {
+					ans += strings.Index(letters(), char) + 1
+					continue GroupsLoop
+				}
+			}
+		}
+	}
 
 	return ans
 }
