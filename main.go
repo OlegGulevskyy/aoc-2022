a727678d5d14810e96b8c2d99d63f57ffbbfddf9
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-04 10:33:08 +0100

    upd maint

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"log"
-
-	// "github.com/OlegGulevskyy/aoc2022/days/day1"
-	// "github.com/OlegGulevskyy/aoc2022/days/day2"
+	"github.com/OlegGulevskyy/aoc2022/days/day1"
+	"github.com/OlegGulevskyy/aoc2022/days/day2"
 	"github.com/OlegGulevskyy/aoc2022/days/day3"
+	"github.com/OlegGulevskyy/aoc2022/days/day4"
 )
 
 func main() {
 	log.Println("Start the AOC xoxo")
 
-	// day1.Solution()
-	// day2.Solution()
+	day1.Solution()
+	day2.Solution()
 	day3.Solution()
+	day4.Solution()
 }
