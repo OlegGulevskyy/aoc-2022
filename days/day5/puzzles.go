13c7699513120aca7141c11eb11ec5b3293c9e8c
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-06 18:27:02 +0100

    day5 passed

--- days/day5/puzzles.go
+++ days/day5/puzzles.go
@@ -1,7 +1,6 @@
 package day5
 
 import (
-	"fmt"
 	"sort"
 	"strconv"
 	"strings"
@@ -40,11 +39,17 @@ func p1(in string) string {
 		}
 	}
 
-	fmt.Println("Containers parsed => ", containers)
+	keys := make([]string, 0, len(containers))
+	for k := range containers {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
 	for _, instr := range instructions {
-		amount, _ := strconv.Atoi(string(instr[5]))
-		srcStr := string(instr[12])
-		destStr := string(instr[17])
+		args := strings.Split(instr, " ")
+		amount, _ := strconv.Atoi(args[1])
+		srcStr := string(args[3])
+		destStr := string(args[5])
 
 		for k, c := range containers {
 			if k != srcStr {
@@ -64,23 +69,69 @@ func p1(in string) string {
 
 	}
 
-	keys := make([]string, 0, len(containers))
-	for k := range containers {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
 	for _, k := range keys {
 		if len(containers[k]) > 0 {
 			ans += containers[k][0]
 		}
 	}
 
-	fmt.Println("Containers after instructions => ", containers)
-
 	return ans
 }
 
-func p2(in string) int {
-	var ans int
+func p2(in string) string {
+	var ans string
+	lines := inpututils.Lines(in)
+	var instructions []string
+
+	containers := map[string][]string{}
+
+	// parse containers
+	for lineIndex, l := range lines {
+		if strings.Index(l, "1") != -1 {
+			for charIndex, c := range l {
+				char := string(c)
+				if char == " " {
+					continue
+				}
+
+				for i := 0; i < lineIndex; i++ {
+					container := string(lines[i][charIndex])
+					if container == "" || container == " " {
+						continue
+					}
+
+					containers[char] = append(containers[char], container)
+				}
+
+			}
+			instructions = lines[lineIndex + 2:]
+			break
+		}
+	}
+
+	keys := make([]string, 0, len(containers))
+	for k := range containers {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, instr := range instructions {
+		args := strings.Split(instr, " ")
+		amount, _ := strconv.Atoi(args[1])
+		srcStr := string(args[3])
+		destStr := string(args[5])
+		temp := append([]string{}, containers[srcStr][:amount]...)
+
+		containers[destStr] = append(temp, containers[destStr]...)
+		containers[srcStr] = containers[srcStr][amount:]
+
+	}
+
+	for _, k := range keys {
+		if len(containers[k]) > 0 {
+			ans += containers[k][0]
+		}
+	}
+
 	return ans
 }
