d00431791ae90731d6e77683fef6439ef58a3b9d
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-07 22:49:22 +0100

    wired up day 7

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"github.com/OlegGulevskyy/aoc2022/days/day1"
 	"github.com/OlegGulevskyy/aoc2022/days/day2"
 	"github.com/OlegGulevskyy/aoc2022/days/day3"
 	"github.com/OlegGulevskyy/aoc2022/days/day4"
 	"github.com/OlegGulevskyy/aoc2022/days/day5"
 	"github.com/OlegGulevskyy/aoc2022/days/day6"
+	"github.com/OlegGulevskyy/aoc2022/days/day7"
 )
 
 func main() {
-	log.Println("Start the AOC xoxo")
+	fmt.Println("Start the AOC xoxo")
+	fmt.Println(" ")
 
 	day1.Solution()
 	day2.Solution()
@@ -19,4 +21,5 @@ func main() {
 	day4.Solution()
 	day5.Solution()
 	day6.Solution()
+	day7.Solution()
 }
